40442fc87e4f370a1bcb66e57b2ecf9626cb29e3
SLimBook <30815731+slimlime@users.noreply.github.com>
2018-05-15 16:44:12 +1000

    ENglish locale option!

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 import app from './app.ui'
 
 let options = {
-  app: app
+  app: app,
+  i18n: {
+    locale: 'en',
+    components: require('./i18n/components.json'),
+    messages: {
+      'zh-cn': require('./i18n/zh-cn.json'),
+      'en': require('./i18n/en.json')
+    }
+  }
 }
 ui.start(options)
